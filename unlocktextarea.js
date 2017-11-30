75de03e099a271cb2a6ec2b6bdd7bf261dd0d4b8
Gyuhyeon <mark3236@naver.com>
2017-11-30 22:23:22 +0900

    ver 0.2 : unlock textareas when the extension button is pressed

--- unlocktextarea.js
+++ unlocktextarea.js
@@ -1,4 +1,6 @@
-let elemlist = document.getElementsByTagName('textarea');
-for (let i=0; i<elemlist.length; ++i) {
-    elemlist[i].style.resize = 'both';
-}
+chrome.browserAction.onClicked.addListener((tab) => {
+    let code = "let elemlist = document.getElementsByTagName('textarea'); for(let i=0; i<elemlist.length; ++i){elemlist[i].style.resize = 'both';}";
+    chrome.tabs.executeScript({
+        code: code
+    });
+});
